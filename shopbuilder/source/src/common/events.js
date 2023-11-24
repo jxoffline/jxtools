22c1e579366b61725b729f23ae0f74d0d8fb8094
Vinh TTN <ryan.nguyen@minasolution.com>
2023-11-24 22:00:30 +0000

    Them OnlineGMPassTool

--- shopbuilder/source/src/common/events.js
+++ shopbuilder/source/src/common/events.js
@@ -1,20 +1,3 @@
-/*
-    Ry: Zendesk starts to have issue with .trigger() .on()
-    It could be because we use name with "." that causes issue in it
-        e.g. 
-        
-        .on("ll.setPinWindow",...)         
-        .on("main.setPinWindow",...) 
-
-        dont receive any event/data with
-        .trigger("ll.setPinWindow")
-        .trigger("main.setPinWindow")
-
-    To avoid such thing, we stop using ZAFv2 for internal app events, but do it ourself here
-
-    We bind it to window.llEvents for global scope to use across all apps (e.g. Q and Seek)
-
-*/
 const events = {};
 const eventsWKey = {};
 
